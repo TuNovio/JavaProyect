e91c7826ee92da73ba5294e8adf59247edab0cba
Alfonso Gomajoa <Alfonso.alfgomachi@gmail.com>
2021-10-27 17:00:15 -0500

    Clase representativa de un elemento correjida en sintaxis

--- aplicacion/Raton.java
+++ aplicacion/Raton.java
@@ -1,15 +1,16 @@
-public class Raton extends DispositivoEntrada{
-    //ATRIBUTOS 
+public class Raton extends DispositivoEntrada {
+    // ATRIBUTOS
     private final int idRaton;
     private static int contadorRaton;
 
-    //CONSTURCTOR
-    public Raton(String nTipo, String nMarca){
-	super(nTipo, nMarca);
-	idRaton = ++Raton.contadorRaton;
+    // CONSTURCTOR
+    public Raton(String nTipo, String nMarca) {
+        super(nTipo, nMarca);
+        idRaton = ++Raton.contadorRaton;
     }
-    //METODOS
+
+    // METODOS
     public String toString(){
-	return "Raton {"+ "idRaton ="+ idRaton+","+ super.toString() "}"
+	return "Raton {"+ "idRaton ="+ idRaton+","+ super.toString()+"}";
     }
 }
